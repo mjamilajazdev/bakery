fd1f5bc1f1856a7fca5c5efa2fc68498864ec662
Muhammad Jamil <jamdevs97@gmail.com>
2022-08-31 06:00:33 +0500

    rubocop is cleaned and specs passed

--- spec/factories/user_factories.rb
+++ spec/factories/user_factories.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'faker'
 FactoryBot.define do
   factory :user do
     full_name { Faker::Company.bs }
-    email {Faker::Internet.email }
-    password  { '123456' }
+    email { Faker::Internet.email }
+    password { '123456' }
   end
 end
